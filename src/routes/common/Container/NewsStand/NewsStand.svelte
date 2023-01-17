65d71da568f17254ceeffe1b3b699ab03ad8f431
leemyeongjae <leemyeongjae@192.168.nate.com>
2023-01-17 14:52:34 +0900

    FEAT :: NewsStand 섹션에 컨텐츠 정보란 제작

--- src/routes/common/Container/NewsStand/NewsStand.svelte
+++ src/routes/common/Container/NewsStand/NewsStand.svelte
@@ -1,9 +1,11 @@
 <script>
     import NewsStandHeader from "./NewsStandHeader/NewsStandHeader.svelte";
+    import NewsStandSection from "./NewsStandSection/NewsStandSection.svelte";
 
 </script>
 <section class="news-stand">
     <NewsStandHeader />
+    <NewsStandSection />
 </section>
 
 <style>
